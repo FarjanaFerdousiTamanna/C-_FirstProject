872502040f8cb509ff556b66d977ac23896fc3f1
FarjanaFerdousiTamanna <farjana.ferdousi.tamanna@gmail.com>
2020-08-18 00:54:19 +0600

    Add project files.

---
+++ FirstProject/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace FirstProject
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*  variable can be set by using var,
+             But good practice is setting the variable name by the datatype
+             ones the datatype is set, it does not need to mention again
+             we can find different methods by going "view- object browser"
+             like the ToUpper is a method
+             maintaining the lowercase uppercase in variable name is called "Camel case"
+             
+             */
+            var name = "tamanna";
+            string newname = "Rakib";
+            newname = newname.ToUpper();
+            Console.WriteLine(name);
+            Console.WriteLine(newname);
+        }
+    }
+    
+}
